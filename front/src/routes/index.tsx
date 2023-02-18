fe0f37162b569d4ef4ace29daa548c8de754615e
Ayrlon Douglas <ayr.l@hotmail.com>
2023-02-18 01:47:49 -0300

    ajustes para build

--- front/src/routes/index.tsx
+++ front/src/routes/index.tsx
@@ -3,15 +3,12 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import ProtectedRoute from "./privateRoute";
 import { LOCAL } from "../helper/constants/localStorage";
 
-import Home from "../pages/Home";
-import Login from "../pages/Login";
 import OrderManagement from "../pages/Order/Management";
 import ItemsList from "../pages/Items/List";
 import EmployeesList from "../pages/Employees/List";
-// import Login from "../pages/Login";
+import Home from "../pages/Home";
+import Login from "../pages/Login";
 
-// import ProfessionalList from "../pages/Professional/List";
-// import Settings from "../pages/Settings";
 export default function SwitchRoutes() {
 	const token = localStorage.getItem(LOCAL.token);
 	return (
